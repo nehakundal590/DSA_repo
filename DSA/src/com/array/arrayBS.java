9fe5474057136e29f09dcb20e12df81e8b09f226
neha <devineha590@gmail.com>
2026-03-20 15:16:18 +0530

    array

---
+++ DSA/src/com/array/arrayBS.java
@@ -0,0 +1,30 @@
+package com.array;
+
+public class arrayBS {
+	public static int binaryarray(int numbers[],int key) {
+		int start=0,end=numbers.length-1;
+		while(start<=end-1) {
+		int	mid=(start+end)/2;
+				if(numbers[mid]==key) {
+					return mid;
+			}
+				if(numbers[mid]<=key) { // right
+					start=mid+1;
+				}
+				else {                   // left
+					end=mid-1;
+					
+				}
+			
+			
+		}
+		return -1;   // not exists
+		
+	}
+	public static void main(String[] args) {
+		int numbers[]= {2,4,6,8,10,12};
+		int key=20;
+		System.out.println("index of numbers"+binaryarray(numbers,key));
+	}
+
+}
---
+++ DSA/src/com/array/arrayBSexample.java
@@ -0,0 +1,38 @@
+package com.array;
+
+public class arrayBSexample {
+	public static int binaryarray(int numbers[],int key) {
+		int start=0,end=numbers.length-1;
+		while(start<=end) {
+		int	mid=(start+end)/2;
+				if(numbers[mid]==key) {
+					return mid;
+			}
+				if(numbers[mid] < key) {  
+				    end = mid - 1;   // go left
+				} else {             
+				    start = mid + 1; // go right
+				}
+			/*	if(numbers[mid]<=key) { // right
+					start=mid+1;
+				}
+				else {                   // left
+					end=mid-1;
+					
+				}*/
+			
+			
+		}
+		return -1;   // not exists
+		
+	}
+	public static void main(String[] args) {
+		int numbers[]= {80,70,45,39,20,19,14};
+		int key=14;
+		System.out.println("index of numbers"+binaryarray(numbers,key));
+	}
+
+}
+
+
+
